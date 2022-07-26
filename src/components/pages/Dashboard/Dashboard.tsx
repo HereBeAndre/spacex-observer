89921ad22f2d24cf89fbd3c8ba2384a41f05aeff
HereBeAndre <andrea.tota6@gmail.com>
2022-07-26 19:09:44 +0200

    Migrate React Query from v3 to V4. Install React Query Devtools v4. Fix deps version in package.json

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useQueryClient } from 'react-query';
+// import { useQueryClient } from '@tanstack/react-query';
 import { RouteComponentProps } from '@reach/router';
 
 import { getLatestLaunch, getNextLaunch, getUpcomingLaunches } from 'api';
